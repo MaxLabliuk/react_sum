e69aa58e48cc5e93530dcf152204d414f620e282
Iuliia Tsentylo <labliuk.maksym@gmail.com>
2020-02-11 13:13:29 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0be2a4d51bf87742479c1b1baf41cd4d.js"
+  "./precache-manifest.b8a11ca14c878fa6a5e2093019d9c6f3.js"
 );
 
 self.addEventListener('message', (event) => {
